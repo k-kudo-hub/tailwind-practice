42d3636bee2cc13b3c9db80722b5d57cc5693243
k-kudo-hub <tongariisaibou@gmail.com>
2021-08-04 14:43:56 +0900

    Changed to Vue

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# tailwind-practice
-This repository is for practicing Tailwind CSS.
+# tailwind-vue-practice
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template class="font-sans">
+  <section class="mx-auto bg-blue-300 h-20 flex justify-center items-center">
+    <div class="sm:container flex h-20">
+      <div class="w-1/3 h-20 flex justify-center items-center">
+        <h1 class="text-4xl text-center text-white font-bold">Tailwind</h1>
+      </div>
+      <div class="w-2/3 flex">
+        <div class="w-1/3 flex justify-center items-center hover:bg-blue-400 rounded-md px-2">
+          <a href="#" class="text-white text-lg">blog <i class="fas fa-sort-down"></i></a>
+        </div>
+        <div class="w-1/3 flex justify-center items-center hover:bg-blue-400 rounded-md px-2">
+          <a href="#" class="text-white text-lg">components <i class="fas fa-sort-down"></i></a>
+        </div>
+        <div class="w-1/3 flex justify-center items-center hover:bg-blue-400 rounded-md px-2">
+          <a href="#" class="text-white text-lg">cheats sheet <i class="fas fa-sort-down"></i></a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
