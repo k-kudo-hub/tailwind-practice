c59878a550e243d698edd800121dd70cd15a5fb0
k-kudo-hub <tongariisaibou@gmail.com>
2021-08-06 20:34:33 +0900

    Added search bar to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,13 @@
 <template>
   <section class="mx-auto bg-blue-300 h-20 flex justify-center items-center">
     <div class="sm:container flex h-20">
-      <div class="w-1/3 h-20 flex justify-center items-center">
+      <div class="w-1/4 h-20 flex justify-center items-center">
         <h1 class="text-4xl text-center text-white font-bold">Tailwind</h1>
       </div>
-      <div class="w-2/3 flex">
+      <div class="w-1/2 flex">
+        <SearchBar/>
+      </div>
+      <div class="w-1/4 flex">
         <PullDown/>
       </div>
     </div>
@@ -13,9 +16,11 @@
 
 <script>
 import PullDown from './PullDown.vue'
+import SearchBar from './SearchBar.vue'
 export default {
   components: {
     PullDown,
+    SearchBar,
   }
 }
 </script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-full flex justify-center items-center">
+    <div class="w-full flex justify-center items-center rounded-3xl bg-blue-400 h-1/2">
+      <input type="text" name="search" class="text-white px-4 py-2 w-10/12 bg-blue-400 placeholder-white" placeholder="Search or jump to ...">
+      <button type="submit" class="w-1/12 bg-blue-400 cursor-pointer">
+        <i class="fas fa-long-arrow-alt-right fa-lg text-gray-200 hover:text-white"></i>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
