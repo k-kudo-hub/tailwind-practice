7d867f3691350f94d62fe5bd2d81e406ec4d474f
k-kudo-hub <tongariisaibou@gmail.com>
2021-08-06 21:03:35 +0900

    Fine adjustment of height and width

--- src/components/PullDown.vue
+++ src/components/PullDown.vue
@@ -2,7 +2,7 @@
   <div class="relative flex justify-center items-center px-2 mx-auto">
     <button @click="toggle" class="text-white">MENU <i class="fas fa-sort-down"></i></button>
     <transition enter-active-class="duration-200 ease-out" enter-from-class="opacity-0 scale-95" enter-to-class="opacity-100 scale-100" leave-active-class="duration-100 ease-in" leave-from-class="opacity-100 scale-100" leave-to-class="opacity-0 scale-95">
-      <ul v-if="isOpen" class="absolute top-16 w-64 p-2 border border-gray-200 bg-blue-300 shadow-md rounded-md">
+      <ul v-if="isOpen" class="absolute top-16 w-72 p-2 border border-gray-200 bg-blue-300 shadow-md rounded-md">
         <li v-for="(item, id) in menu" :key="id" class="w-full y-full">
           <a :href="item.link" class="flex duration-75 focus:bg-blue-400 hover:bg-blue-400 hover:text-white p-2 text-white w-full y-full block rounded-md">
             <div class="w-1/5 text-white flex items-center">
