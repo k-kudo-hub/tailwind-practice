d3816687c9d1e69fb232119e04da4aea76fc14b9
k-kudo-hub <tongariisaibou@gmail.com>
2021-08-06 20:56:25 +0900

    gave the h1 element a link to the top page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,11 @@
   <section class="mx-auto bg-blue-300 h-16 flex justify-center items-center">
     <div class="sm:container flex h-20">
       <div class="w-1/4 h-20 flex justify-center items-center">
-        <h1 class="text-4xl text-center text-white font-bold">Tailwind</h1>
+        <h1 class="text-4xl text-center text-white font-bold">
+          <a href="/">
+            Tailwind
+          </a>
+        </h1>
       </div>
       <div class="w-1/2 flex">
         <SearchBar/>
