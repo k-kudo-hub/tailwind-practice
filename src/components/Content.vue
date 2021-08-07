71403a968993c16b09cbd66830168a4a01c696cf
k-kudo-hub <tongariisaibou@gmail.com>
2021-08-07 10:04:12 +0900

    Initialized some Vue file

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,21 +1,27 @@
 <template>
   <section class="h-content flex">
     <div class="w-1/4 min-h-full bg-gray-50 py-5 px-2">
-
+      <Selfintroduce/>
     </div>
     <div class="w-1/2 min-h-full bg-white py-5 px-2">
-
+      <ProjectList/>
     </div>
     <div class="w-1/4 min-h-full bg-gray-50 py-5 px-2">
-
+      <ActivityLog/>
     </div>
   </section>
 </template>
 
 <script>
-
+import SelfIntroduce from './SelfIntroduce.vue'
+import ProjectList from './ProjectList.vue'
+import ActivityLog from './ActivityLog.vue'
 export default {
-  
+  components: {
+    SelfIntroduce,
+    ProjectList,
+    ActivityLog,
+  }
 }
 </script>
 
