54a3c84a90581a0d85653b3bac521ea11f264624
k-kudo-hub <tongariisaibou@gmail.com>
2021-08-10 20:32:28 +0900

    Created a list of projects

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="h-20 border-gray-100 border-b-2 flex items-center">
-    <h2 class="w-1/6 text-xl font-bold pl-2">Projects</h2>
+  <header class="h-20 border-gray-100 border-b-2 flex items-center">
+    <h1 class="w-1/6 text-xl font-bold pl-2">Projects</h1>
     <div class="w-3/6 h-1/2 flex items-center">
       <input type="text" class="border border-gray-300 py-1 px-3 rounded-3xl w-11/12 placeholder-gray-400" placeholder="Find a project...">
     </div>
@@ -12,37 +12,62 @@
         <option value="Latest recently updated">Latest recently updated</option>
       </select>
     </div>
-  </div>
-  <div v-for="project in projects" :key="project.id" class="border-gray-100 border-b-2 flex display-col h-32 px-3 py-2">
+  </header>
+  <article v-for="(item, index) in projects" :key="item.id" class="border-gray-100 border-b-2 flex flex-col h-32 px-3 py-2">
     <div class="h-1/2 flex items-center w-full">
-      <h2 class="w-1/2 font-bold text-md">{{ project.title }}</h2>
-      <p class="w-1/2">Visit site </p>
+      <h1 class="w-1/2 font-bold text-md">
+        <a href="#" class="cursor-pointer hover:text-gray-500">{{ item.title }}</a>
+      </h1>
+      <div class="w-1/4 text-right">
+        <a href="#" class="text-gray-500 hover:text-black">Visit site</a>
+      </div>
+      <div class="w-1/4 text-right pr-5">
+        <button v-if="!item.stared" @click="toggleStar(index)"><i class="far fa-star cursor-pointer text-gray-500 hover:text-black"></i></button>
+        <button v-else @click="toggleStar(index)"><i class="fas fa-star cursor-pointer text-yellow-400 hover:text-yellow-300"></i></button>
+      </div>
     </div>
-    <div class="h-1/2 flex items-center">
+    <div class="h-1/2 flex items-center w-full text-gray-500">
+      <div class="w-1/2">
+        <p><i class="fab fa-github fa-lg mr-2"></i>{{ user.id }}/{{ item.id }}</p>
+      </div>
+      <div class="w-1/2 pr-5 flex justify-end items-center">
+        <p class="mr-3">{{ item.language }}</p>
+        <p class="mr-3">・</p>
+        <p>Updated {{ item.updated_at }}</p>
+      </div>
     </div>
-  </div>
+  </article>
 </template>
 
 <script>
 export default {
   data(){
     return {
+      user: {
+        id: "tailwind"
+      },
       projects: [
         {
           title: "Tailwind Practice",
           stared: true,
           id: "tailwind-practice",
           language: "Vue.js",
-          updated_at: "2h",
+          updated_at: "2h ago",
         },
         {
           title: "Vue3 Practice",
           stared: false,
           id: "vue3-practice",
           language: "Vue.js",
-          updated_at: "4h",
+          updated_at: "4h ago",
         }
       ]
+    }
+  },
+  methods: {
+    toggleStar(index){
+      const project = this.projects[index]
+      project.stared ? project.stared = false : project.stared = true
     }
   }
 }
